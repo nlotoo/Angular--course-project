826068072906a08e4e943039a65828fbd12f9f47
Yordan Nikolov <jnikolow93@gmail.com>
2022-11-05 07:07:18 +0000

    update home-page and navigation

--- Angular-course-project/src/app/app.module.ts
+++ Angular-course-project/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopHeadNavigationComponent } from './header-elements/top-head-navigation/top-head-navigation.component';
-
+import { TopHeadNavigationComponent } from './header-elements/top-head-navigation/home-page/navigation-element/top-head-navigation.component'
+import { HomePageComponent } from './header-elements/top-head-navigation/home-page/home-page/home-page.component';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatSliderModule } from '@angular/material/slider';
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
     TopHeadNavigationComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
