a5750d218c588bde4b824d0d58f6cea223359a85
Yordan Nikolov <jnikolow93@gmail.com>
2022-11-26 07:53:15 +0000

    downgrade

--- CoffeeLovers/client/karma.conf.js
+++ CoffeeLovers/client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/washup'),
+      dir: require('path').join(__dirname, './coverage/washAPP'),
       subdir: '.',
       reporters: [
         { type: 'html' },
